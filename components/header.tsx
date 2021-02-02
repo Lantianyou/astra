bebaf0e43d07d15b88d4df1e5140144032a63f77
Lantianyou <dtlantianyou@gmail.com>
2021-02-02 15:07:28 +0800

    initial commit

---
+++ components/header.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { brandName } from '../constant';
+
+function Header() {
+  return (
+    <header className="flex justify-between max-w-screen-xl mx-auto my-10">
+       <Link href="/"> 
+        <a>{brandName}</a>
+        </Link>
+    <ul className="flex justify-around w-1/3">
+       <Link href="/services"> 
+       <a>Services</a>
+       </Link>
+       <Link href="/about"> 
+       <a>About</a>
+       </Link>
+       <Link href="/team">
+          <a>Team</a>
+          </Link>
+       <Link href="/careers"> 
+       <a>Careers</a>
+       </Link>
+       <Link href="/blog"> 
+       <a>Newsroom</a>
+       </Link>
+    </ul>
+    </header>
+  );
+}
+
+export default Header;
